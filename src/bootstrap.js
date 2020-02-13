15197d230a583df05ff1b7fe51e87038974b24d0
Jeffrey Zelaya <jeffreyz.dev@gmail.com>
2020-02-12 22:41:28 -0700

    initial commit

---
+++ src/bootstrap.js
@@ -0,0 +1,7 @@
+// - Inside the bootstrap.js file I want you to import the classChallenge file and use the default function along with the two named exports
+// - console.log() the default function along with the two named exports
+import classChallenge, {greeting, numSum} from './classChallenge'
+
+console.log(classChallenge)
+console.log(greeting)
+console.log(numSum(1,2))
---
+++ src/classChallenge.js
@@ -0,0 +1,12 @@
+export default function () {
+    return "<div>classChallenge</div>"
+}
+
+export var greeting = "Welcome"
+
+export function numSum(num1, num2) {
+    return num1+num2
+} 
+// default function needs to return `<div>classChallenge</div>`
+// one of the named exports needs to be a variable that has a value of a string “welcome”
+// one of the named exports needs to be a function that returns the sum of two numbers
